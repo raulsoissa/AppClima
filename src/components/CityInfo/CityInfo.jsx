a9096bcf4f1936b9c8b9e802ab6ca57ca94ddebf
raul soissa <raulsoissa@gmail.com>
2020-07-22 19:14:08 -0400

    final video 237

---
+++ src/components/CityInfo/CityInfo.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const CityInfo = ({ city, country }) => {
+    return (
+        <div className="">
+
+        </div>
+    )
+}
+
+CityInfo.propTypes = {
+    city: PropTypes.string.isRequired,
+    country: PropTypes.string.isRequired,
+}
+
+export default CityInfo
+
