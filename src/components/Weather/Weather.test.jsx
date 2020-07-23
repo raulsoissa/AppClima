813ca98ac40469c49807cba251a42759e5b2d781
raul soissa <raulsoissa@gmail.com>
2020-07-23 10:25:46 -0400

    final video 248

---
+++ src/components/Weather/Weather.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { render } from '@testing-library/react';
+import Weather from './Weather'
+import '@testing-library/jest-dom/extend-expect'
+
+test("Weather render", async () => {
+    const { findByRole } = render(<Weather temperature={10}/>)
+
+    const temp = await findByRole("heading")
+
+    expect(temp).toHaveTextContent("10")
+})
