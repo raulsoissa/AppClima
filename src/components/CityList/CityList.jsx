4c61288dd8b0dc5b90e0bbb09e0a1e34dbe6a546
raul soissa <raulsoissa@gmail.com>
2020-07-24 08:53:00 -0400

    final video 266

--- src/components/CityList/CityList.jsx
+++ src/components/CityList/CityList.jsx
@@ -10,10 +10,10 @@ const renderCityAndCountry = CityAndCountry => {
     return (
         <li key={city}>
             <Grid container justify="center" alignItems="center">
-                <Grid item sm={8}>
+                <Grid item md={8} xs={12}>
                     <CityInfo city={city} country={country}/>
                 </Grid>
-                <Grid item sm={4}>
+                <Grid item md={4} xs={12}>
                     <Weather temperature={10} state="sunny"/> 
                 </Grid>
             </Grid>
