64129e07f06d2ac8667f9113ba18b4f66e1b50cb
raul soissa <raulsoissa@gmail.com>
2020-07-24 09:34:33 -0400

    final video 272

--- src/components/CityList/CityList.jsx
+++ src/components/CityList/CityList.jsx
@@ -3,12 +3,13 @@ import PropTypes from 'prop-types'
 import Grid from '@material-ui/core/Grid'
 import CityInfo from '../CityInfo/'
 import Weather from '../Weather/Weather'
+import { List, ListItem } from '@material-ui/core'
 
-const renderCityAndCountry = eventOnClickCity=> CityAndCountry => {
+const renderCityAndCountry = eventOnClickCity=> CityAndCountry => { // esto es = a esto -> = (eventOnClickCity, CityAndCountry) =>
     const { city, country } = CityAndCountry
 
     return (
-        <li key={city} onClick={eventOnClickCity}>
+        <ListItem key={city} onClick={eventOnClickCity}>
             <Grid container justify="center" alignItems="center">
                 <Grid item md={8} xs={12}>
                     <CityInfo city={city} country={country}/>
@@ -17,17 +18,18 @@ const renderCityAndCountry = eventOnClickCity=> CityAndCountry => {
                     <Weather temperature={10} state="sunny"/> 
                 </Grid>
             </Grid>
-        </li>
+        </ListItem>
     )
 }
 
 const CityList = ({ cities, onClickCity }) => {
+
     return (
-        <ul>
+        <List>
             {
                 cities.map(CityAndCountry => renderCityAndCountry(onClickCity)(CityAndCountry))
             }
-        </ul>
+        </List>
     )
 }
 
