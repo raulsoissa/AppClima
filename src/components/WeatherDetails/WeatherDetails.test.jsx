7b4bc7874cce769e62a1c27e1a6ea32735aa2fac
raul soissa <raulsoissa@gmail.com>
2020-07-24 09:54:44 -0400

    final video 274

--- src/components/WeatherDetails/WeatherDetails.test.jsx
+++ src/components/WeatherDetails/WeatherDetails.test.jsx
@@ -1,3 +1,14 @@
-test("WeatherDetails render", () => {
-    
+import React from 'react'
+import { render } from '@testing-library/react'
+import WeatherDetails from './WeatherDetails'
+
+test("WeatherDetails render", async () => {
+    const { findByText } = render(<WeatherDetails humidity={10} wind={2}/>)
+
+    const wind = await findByText(/2/)
+    const humidity = await findByText(/10/)
+
+    expect(wind).toHaveTextContent("Viento 2 km/h")
+    expect(humidity).toHaveTextContent("Humedad 10 %")
+
 })
