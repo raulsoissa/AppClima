ba9394b7663be48f7c040af32edd11ca59fb96af
raul soissa <raulsoissa@gmail.com>
2020-07-25 09:50:48 -0400

    final video 298

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 const App = () => {
     return (
         <div>
-            App
+            <h1>App</h1>
+            <Router>
+                <Switch>
+                    <Route path="/main">Main</Route>
+                    <Route path="/city">City</Route>
+                    <Route exact path="/">Welcome</Route>
+                </Switch>
+            </Router>
         </div>
     )
 }
