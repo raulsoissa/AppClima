c37baf41fde4e2fc374149ea4c8ce096d7140ce5
raul soissa <raulsoissa@gmail.com>
2020-07-25 09:55:53 -0400

    final video 299

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
 
 const App = () => {
     return (
         <div>
             <h1>App</h1>
             <Router>
+                <div className="">
+                    <Link to="/main">Ir a main</Link>
+                </div>
                 <Switch>
+                    <Route exact path="/">Welcome</Route>
                     <Route path="/main">Main</Route>
                     <Route path="/city">City</Route>
-                    <Route exact path="/">Welcome</Route>
+                    <Route>Not Found</Route>
                 </Switch>
             </Router>
         </div>
