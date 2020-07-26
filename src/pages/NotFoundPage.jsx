2f5f01bacac369ae3d2751188d4d1e076f032cc2
raul soissa <raulsoissa@gmail.com>
2020-07-26 10:23:37 -0400

    final video 341

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,15 +1,33 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from '@material-ui/core'
+import Grid from '@material-ui/core/Grid'
+import { IconContext } from 'react-icons'
+import { WiStormShowers } from 'react-icons/wi'
+import { Typography } from '@material-ui/core'
+import {Link as RouterLink} from 'react-router-dom'
 
 
 const NotFoundPage = () => {
     return (
-        <div>
-            Not Found
-            <div className="">
-                <Link to="/main">Volver a main</Link>
-            </div>
-        </div>
+        <Grid container direction="column" justify="center" className="full">
+                <div className="highlight">
+                    <Grid item container xs={12} justify="center" alignItems="center">
+                        <Grid>
+                            <IconContext.Provider value={{ size:"6em"}}>
+                                <WiStormShowers/>
+                            </IconContext.Provider>
+                        </Grid>
+                        <Grid item container direction="column" justify="center" alignItems="center">
+                            <Typography variant="h4" color="inherit">
+                                404 | La página no existe
+                            </Typography>
+                            <Link color="inherit" aria-label="menu" component={RouterLink} to="/">
+                                Volver
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </div>
+            </Grid>
     )
 }
 
