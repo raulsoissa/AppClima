f30682fca8c6f06f3cf2ec302f48dd064e47dfef
raul soissa <raulsoissa@gmail.com>
2020-07-26 09:47:47 -0400

    final video 333

--- src/WelcomeScreen/WelcomeScreen.jsx
+++ src/WelcomeScreen/WelcomeScreen.jsx
@@ -1,15 +1,32 @@
-import React, {useRef, useEffect} from 'react'
+import React, {useRef, useEffect, useState} from 'react'
 import PropTypes from 'prop-types'
+import Clouds from 'vanta/dist/vanta.clouds.min'
+import * as THREE from 'three'
 
 
 const WelcomeScreen = ({ children }) => {
 
     const myRefDiv = useRef(null)
+    const [vanta, setVanta] = useState(0)
+
     console.log(" myRefDiv.current ",myRefDiv.current)
 
     useEffect(() => {
-        console.log(" myRefDiv.current useEffect",myRefDiv.current)
-    })
+        if (!vanta) {
+            setVanta(Clouds({
+                THREE,
+                el: myRefDiv.current
+            }))
+        }
+        return () => {
+            if (vanta) {
+                vanta.destroy()
+                console.log("clean up")
+            }
+        }
+    }, [vanta])
+
+
 
     return (
         <div ref={myRefDiv}>
