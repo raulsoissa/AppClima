63ba965525b28c82230271272d640d6f2edade56
raul soissa <raulsoissa@gmail.com>
2020-08-03 23:49:06 -0400

    final video 363

--- src/components/CityList/CityList.jsx
+++ src/components/CityList/CityList.jsx
@@ -35,32 +35,26 @@ const CityList = ({ cities, onClickCity }) => {
     const [error, setError] = useState(null)
 
     useEffect(() => {
-        const setWeather = (city, country, countryCode) => {
+        const setWeather = async (city, country, countryCode) => {
             const api_key ="adb866c0cb646005a389d2f8e9268761"
-            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}, ${countryCode}&appid=${api_key}`
-            axios
-                .get(url)
-                .then(response => {
-                    const { data } = response
-                    const temperature = convertUnits(data.main.temp).from("K").to("C").toFixed(0)
-                    const state = data.weather[0].main.toLowerCase()
-                    const propName = `${city}-${country}`
-                    const propValue = { temperature,state }
-
-                   setAllWeather(allWeather => ({...allWeather, [propName]: propValue }))
-                })
-                .catch(error => {
-                    if (error.response) {
-                        const {data, status} = error.response
-                        console.log("data", data)
-                        console.log("status", status)
-                        setError("Ha ocurrido un error en el servidor del clima")
-                    } else if (error.request) {
-                        setError("Verifique su conexión a internet")
-                    } else {
-                        setError("Error al cargar los datos")
-                    }
-                })
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&appid=${api_key}`
+            try {
+                const response = await axios.get(url) 
+                const { data } = response
+                const temperature = convertUnits(data.main.temp).from("K").to("C").toFixed(0)
+                const state = data.weather[0].main.toLowerCase()
+                const propName = `${city}-${country}`
+                const propValue = { temperature,state }
+                setAllWeather(allWeather => ({...allWeather, [propName]: propValue }))
+            } catch (error) {
+                if (error.response) {
+                    setError("Ha ocurrido un error en el servidor del clima")
+                } else if (error.request) {
+                    setError("Verifique su conexión a internet")
+                } else {
+                    setError("Error al cargar los datos")
+                }
+            }
         }
 
         cities.forEach(({ city, country, countryCode }) => {
